fa62bebb580dde097d1644fc74321106fde9bca8
mukandalee <leonardmukanda254@gmail.com>
2023-01-23 08:03:24 +0300

    partiall completed

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -4,8 +4,29 @@ function ItemForm() {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+    console.log(itemData);
+    
+    fetch("http://localhost:4000/items", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(itemData),
+  })
+    .then((r) => r.json())
+    .then((newItem) => console.log(newItem));
+  }
+
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
